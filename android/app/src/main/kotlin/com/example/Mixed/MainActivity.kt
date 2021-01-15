44075a27550281678dbe374149a8ca6d9a470e7a
Vignesh Ganesan <vicky08rockz@gmail.com>
2021-01-15 17:31:46 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/Mixed/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Mixed
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'screens/tab_screen.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TabScreen(),
+    );
+  }
+}
